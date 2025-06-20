da600cbd0573199967ebb7a4374734a2052b1a15
JoMortimer <joanne.mortimer@ligentia.global>
2025-06-20 13:39:47 +0100

    Made changes to Login file

--- Timesheet.Test/Unit/AuthDBTest.cs
+++ Timesheet.Test/Unit/AuthDBTest.cs
@@ -1,25 +1,25 @@
 namespace Timesheet.Test.Unit;
-using Timesheet.Models.Auth;
 using Timesheet.DB;
+using Timesheet.Models.Auth;
 
 public class AuthDBTest
 {
 
-   private AuthDB _authDB = new AuthDB();
+    private AuthDB _authDB = new AuthDB();
 
-   [SetUp]
-   public void Setup()
-   {
-       PrepareDB prepareDB = new PrepareDB();
-       prepareDB.SeedUsers();
-   }
+    [SetUp]
+    public void Setup()
+    {
+        PrepareDB prepareDB = new PrepareDB();
+        prepareDB.SeedUsers();
+    }
 
-   [Test]
-   public void CheckLoginReturnsPostiveResult()
-   {
-       LoginResult loginResult = _authDB.CheckLogin("admin@test.com", "password123");
+    [Test]
+    public void CheckLoginReturnsPositiveResult()
+    {
+        LoginResult loginResult = _authDB.CheckLogin("admin@test.com", "password123");
 
-       Assert.That(loginResult.isUser, Is.True);
-   }
+        Assert.That(loginResult.isUser, Is.True);
+    }
 
 }
